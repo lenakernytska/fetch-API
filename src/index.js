8fb526dc9d04c7686320458751cde7df64a729bb
lenakernytska <yaapril222@gmail.com>
2021-02-02 18:55:57 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './styles.css';
 // import showLightbox from "./js/lightbox"
 import galleryTpl from "./templataes/gallery.hbs"
 import genres from "./js/genres";
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
 
 const refs = {
   galleryRef: document.querySelector(".gallery"),
@@ -159,7 +162,7 @@ function updateGalleryMarkup(results) {
     let newGenres = [];
     item.genre_ids.map((id) => {
       const found = genres.find((item) => item.id === id);
-      console.log(found)
+ 
       newGenres.push(found.name);
     });
     if (newGenres.length >= 3) {
@@ -175,4 +178,60 @@ function updateGalleryMarkup(results) {
   });
     const galleryMarkup = galleryTpl(results);
     refs.galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
- }
+}
+ 
+
+var container = document.getElementById('pagination');
+var options = { // below default value of options
+     totalItems: 20000,
+     itemsPerPage: 20,
+     visiblePages: 8,
+     page: 1,
+     centerAlign: false,
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+     template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
+};
+var pagination = new Pagination(container, options);
+
+const paginRef = document.querySelector("#pagination");
+console.log(paginRef);
+
+
+paginRef.addEventListener('click', paginationHandler);
+
+function paginationHandler(event) {
+  console.log(event)
+   if (event.target.nodeName !== 'A') {
+      return;
+   } else {
+     
+        refs.galleryRef.innerHTML = '';
+     const targetВtn = event.target;
+     console.log(targetВtn)
+     event.preventDefault()
+     if (targetВtn.className === 'tui-page-btn') {
+       const targetPage = Number(targetВtn.textContent);
+       fetch(`${baseUrl}/trending/movie/day?&page=${targetPage}&api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(({ results}) => updateGalleryMarkup(results))
+  .catch(error => console.log(error));
+     }
+  
+    }
+}
